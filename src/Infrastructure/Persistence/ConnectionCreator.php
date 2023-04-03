abccab6a502d6ca35cdb8fa7a9ce9e5584195a42
Yago Souza <yagos-s@live.com>
2023-04-03 20:30:55 -0300

    aulas finalizadas

--- src/Infrastructure/Persistence/ConnectionCreator.php
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -8,9 +8,11 @@ class ConnectionCreator
 {
     public static function createConnection(): PDO
     {
-        $databasePath = __DIR__ . '/../../../banco.sqlite';
-
-        $connection = new PDO('sqlite:' . $databasePath);
+        $connection = new PDO(
+            'mysql:host=172.17.0.2;dbname=banco',
+            'root',
+            'senhalura'
+        );
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
